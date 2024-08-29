ce3a5b7dca2e5c6091562b4eaf63338118ba178a
XiaoXiaoXuXu <160853850+XUjun6666@users.noreply.github.com>
2024-08-29 09:17:28 +0800

    1

--- Assets/Editor/LHSControlEditor.cs
+++ Assets/Editor/LHSControlEditor.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class LHSControlEditor : MonoBehaviour
+using UnityEditor;
+using UnityEditor.TerrainTools;
+[CustomEditor(typeof(LHSControl))]
+public class LHSControlEditor : Editor
 {
-    // Start is called before the first frame update
-    void Start()
+    private LHSControl targetComponment;
+    private void OnEnable()
     {
-        
+        targetComponment = (LHSControl)target;
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void OnInspectorGUI()
     {
-        
+        EditorGUILayout.LabelField("屏幕后处理：亮度 饱和度 对比度调整");
+        targetComponment.Brightness = EditorGUILayout.Slider(new GUIContent("屏幕亮度"), targetComponment.Brightness, 0, 5); 
+        targetComponment.Saturation = EditorGUILayout.Slider(new GUIContent("屏幕饱和度"), targetComponment.Saturation, 0, 5);
+        targetComponment.Contrast = EditorGUILayout.Slider(new GUIContent("屏幕对比度"), targetComponment.Contrast, 0, 5);
     }
 }
